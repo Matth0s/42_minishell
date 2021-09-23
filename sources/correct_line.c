c8b8156331acd0697a3abde0422e8812f06281fa
Math0000 <matheus.moreira.jpa@gmail.com>
2021-09-23 00:51:46 -0300

    substi de var mó bagunça mas operando

--- sources/correct_line.c
+++ sources/correct_line.c
@@ -6,11 +6,24 @@
 /*   By: mmoreira <mmoreira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 01:50:38 by mmoreira          #+#    #+#             */
-/*   Updated: 2021/09/22 21:44:21 by mmoreira         ###   ########.fr       */
+/*   Updated: 2021/09/23 00:49:04 by mmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	on_off_quotes(char *line, int quote1, int quote2, char c)
+{
+	if (quote2 == -1)
+	{
+		if (quote1 == -1 && ft_strchr(line + 1, c))
+			return (-1);
+		else if (quote1 == 1)
+			return (-1);
+	}
+	return (1);
+}
+
 
 static void	insert_spaces(char **line, int *i, int ind)
 {
@@ -53,17 +66,29 @@ static void	check_spaces(char **line, int *i)
 	}
 }
 
-int	on_off_quotes(char *line, int quote1, int quote2, char c)
+static void	correct_redirects(char **line)
 {
-	if (quote2 == -1)
+	int		db_quote;
+	int		sp_quote;
+	int		i;
+
+	i = -1;
+	sp_quote = -1;
+	db_quote = -1;
+	while (*(*line + ++i))
 	{
-		if (quote1 == -1 && ft_strchr(line + 1, c))
-			return (-1);
-		else if (quote1 == 1)
-			return (-1);
+		if (*(*line + i) == '\'')
+			sp_quote *= on_off_quotes((*line + i), sp_quote, db_quote, '\'');
+		if (*(*line + i) == '\"')
+			db_quote *= on_off_quotes((*line + i), db_quote, sp_quote, '\"');
+		if (sp_quote == -1 && db_quote == -1)
+			check_spaces(line, &i);
 	}
-	return (1);
 }
+
+
+
+
 
 void	remove_dollar(char **line, int *i)
 {
@@ -90,9 +115,9 @@ void	replace_dollar(char **line, int *i, int quote)
 	char	*key;
 	int		j;
 
-	j = 0;
+	j = 1;
 	while (*(*line + *i + j) != ' ' && *(*line + *i + j) != '='
-			&& *(*line + *i + j) != '\0')
+			&& *(*line + *i + j) != '$' && *(*line + *i + j) != '\0')
 	{
 		if (*(*line + *i + j) == '\'' && ft_strchr((*line + *i + j + 1), '\'' ))
 			break ;
@@ -105,24 +130,32 @@ void	replace_dollar(char **line, int *i, int quote)
 		}
 		j++;
 	}
-	temp = ft_substr(*line, 0, *i);
-	key = ft_substr(*line, *i + 1, j);
+ 	temp = ft_substr(*line, 0, *i);
+	//printf("---%s---\n",temp);
+	key = ft_substr(*line, *i + 1, j - 1);
+	//printf("---%s---\n",key);
 	lst = search_var(key);
+	free(key);
 	if (lst)
 	{
 		temp2 = ft_strjoin(temp, ((t_var *)lst->vol)->value);
+	//	printf("---%s---\n",temp2);
 		free(temp);
 		temp = temp2;
 	}
-	temp2 =ft_strdup(*line + *i + j);
+	//char	*ft_multi_strjoin(int len, ...)
+	temp2 = ft_strdup(*line + *i + j);
+	//printf("---%s---%d\n",*line + *i + j,j);
+	//exit(0);
 	free(*line);
-	if(temp2)
-/* 	*line = ft_strjoin(temp,temp2);
-		printf("%s--\n",*line);
-	exit(0); */
+ 	*line = ft_strjoin(temp, temp2);
+	//exit(0);
 	free(temp);
 	free(temp2);
-	(*i) += j - 1;
+	if(lst)
+		*i = *i + ft_strlen(((t_var *)lst->vol)->value) - 1;
+	else
+		*i = *i -1;
 }
 
 void	check_dollar(char **line, int *i, int quote)
@@ -131,8 +164,6 @@ void	check_dollar(char **line, int *i, int quote)
 	{
 		if (quote == -1 && ft_strchr((*line + *i + 2), '\"'))
 			remove_dollar(line, i);
-/* 		else if (quote == -1 && !(ft_strchr((*line + *i + 2), '\"')))
-			printf("decidir o que fazer\n"); */
 		if (quote == 1)
 			return ;
 	}
@@ -140,8 +171,6 @@ void	check_dollar(char **line, int *i, int quote)
 	{
 		if (ft_strchr((*line + *i + 2), '\''))
 			remove_dollar(line, i);
-/* 		else
-			printf("decidir o que fazer\n"); */
 	}
 	else if (*(*line + *i + 1) == '$')
 	{
@@ -162,18 +191,16 @@ void	correct_line(char **line)
 	int		i;
 
 	i = -1;
-	sp_quote = -1;
 	db_quote = -1;
+	sp_quote = -1;
+	correct_redirects(line);
 	while (*(*line + ++i))
 	{
 		if (*(*line + i) == '\'')
 			sp_quote *= on_off_quotes((*line + i), sp_quote, db_quote, '\'');
 		if (*(*line + i) == '\"')
 			db_quote *= on_off_quotes((*line + i), db_quote, sp_quote, '\"');
-		if (sp_quote == -1 && db_quote == -1)
-			check_spaces(line, &i);
+		if (*(*line + i) == '$' && sp_quote == -1)
+			check_dollar(line, &i, db_quote);
 	}
 }
-
-/* 		if (*(*line + i) == '$' && sp_quote == -1)
-			check_dollar(line, &i, db_quote); */
